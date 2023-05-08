64c8f34b128f441b82c24a6bf87799543383feb9
Shakhzoda Mubinova <shakhzoda.mubinova@gmail.com>
2023-05-07 23:54:15 -0500

    troubleshooting

--- main.py
+++ main.py
@@ -9,21 +9,24 @@ import mysql.connector
  # Connect to the MySQL database
 conn = mysql.connector.connect(
     user="root",
-    database="elite_bank",
+    database="elite_102",
     password="Nafisa@2008"
 )
 cursor = conn.cursor()
-
+balance = 0 
 
 def search_user():
+    global balance
     # Get the entered user ID
     entered_username = user_id_entry.get()
 
     # Check if the user exists in the database
     # query = f"SELECT * FROM users WHERE username = '{entered_username}'"
     query = f"select * from users"
+    cursor = conn.cursor()
     cursor.execute(query)
     user = cursor.fetchone() 
+    cursor.close()
     breakpoint()
 
 
@@ -44,13 +47,14 @@ def search_user():
         error_label.config(text="User not found")
 
 def withdraw():
+    global balance
     # Get the entered withdrawal amount
     withdrawal_amount = float(withdraw_entry.get())
 
     if balance >= withdrawal_amount:
         # Update the balance
         balance -= withdrawal_amount
-        query = f"UPDATE users SET balance = {balance} WHERE id = {user_id}"
+        query = f"UPDATE users SET balance = {balance} WHERE id = {username}"
         cursor.execute(query)
         conn.commit()
 
@@ -60,12 +64,13 @@ def withdraw():
         error_label.config(text="Insufficient funds")
 
 def deposit():
+    global balance
     # Get the entered deposit amount
     deposit_amount = float(deposit_entry.get())
 
     # Update the balance
     balance += deposit_amount
-    query = f"UPDATE users SET balance = {balance} WHERE id = {user_id}"
+    query = f"UPDATE users SET balance = {balance} WHERE id = {username}"
     cursor.execute(query)
     conn.commit()
 
@@ -117,244 +122,3 @@ error_label.pack()
 root.mainloop()
 
 conn.close()
-
-
-# import tkinter as tk
-# import mysql.connector
-
-#  # Connect to the MySQL database
-# conn = mysql.connector.connect(
-#     user="root",
-#     database="elite_bank",
-#     password="Nafisa@2008"
-# )
-# cursor = conn.cursor()
-
-# def login():
-#     # Get the entered username and password
-#     entered_username = username_entry.get()
-#     entered_password = password_entry.get()
-
-#     # Check if the user exists in the database
-#     query = f"SELECT * FROM users WHERE username = '{entered_username}' AND password = '{entered_password}'"
-#     cursor.execute(query)
-#     user = cursor.fetchone()
-
-#     if user:
-#         # Save the user ID and balance for future reference
-#         global user_id, balance
-#         user_id = user[0]
-#         balance = user[3]
-
-#         # Update the balance label
-#         balance_label.config(text=f"Balance: ${balance}")
-
-#         # Enable the transaction buttons
-#         transfer_button.config(state=tk.NORMAL)
-#         check_balance_button.config(state=tk.NORMAL)
-#     else:
-#         # Show an error message
-#         error_label.config(text="Invalid username or password")
-
-# def transfer():
-#     # Get the entered recipient username and transfer amount
-#     recipient_username = recipient_entry.get()
-#     transfer_amount = int(amount_entry.get())
-
-#     # Check if the recipient exists in the database
-#     query = f"SELECT * FROM users WHERE username = '{recipient_username}'"
-#     cursor.execute(query)
-#     recipient = cursor.fetchone()
-
-#     if recipient:
-#         recipient_id = recipient[0]
-#         recipient_balance = recipient[3]
-
-#         if balance >= transfer_amount:
-#             # Update the sender's balance
-#             balance -= transfer_amount
-#             query = f"UPDATE users SET balance = {balance} WHERE id = {user_id}"
-#             cursor.execute(query)
-
-#             # Update the recipient's balance
-#             recipient_balance += transfer_amount
-#             query = f"UPDATE users SET balance = {recipient_balance} WHERE id = {recipient_id}"
-#             cursor.execute(query)
-
-#             # Commit the changes to the database
-#             conn.commit()
-
-#             # Update the balance label
-#             balance_label.config(text=f"Balance: ${balance}")
-#         else:
-#             error_label.config(text="Insufficient funds")
-#     else:
-#         error_label.config(text="Recipient not found")
-
-# def check_balance():
-#     balance_label.config(text=f"Balance: ${balance}")
-
-# # Create the main root
-# root = tk.Tk()
-# root.title("WELCOME TO THE BANK")
-
-# frame = tk.Frame(root, bg='#3e646c')
-# frame.place(relwidth=0.8, relheight=0.9, rely=0.1, relx=0.1 )
-
-
-# # Create and place the login form widgets
-# username_label = tk.Label(root, text="Username:")
-# username_label.pack()
-# username_entry = tk.Entry(root)
-# username_entry.pack()
-
-# password_label = tk.Label(root, text="Password:")
-# password_label.pack()
-# password_entry = tk.Entry(root, show="*")
-# password_entry.pack()
-
-# login_button = tk.Button(root, text="Login", command=login)
-# login_button.pack()
-
-# # Create and place the transaction form widgets
-# recipient_label = tk.Label(root, text="Recipient:")
-# recipient_label.pack()
-# recipient_entry = tk.Entry(root)
-# recipient_entry.pack()
-
-# amount_label = tk.Label(root, text="Amount:")
-# amount_label.pack()
-# amount_entry = tk.Entry(root)
-# amount_entry.pack()
-
-# transfer_button = tk.Button(root, text="Transfer", command=transfer, state=tk.DISABLED)
-# transfer_button.pack()
-
-# check_balance_button = tk.Button(root, text="Check Balance", command=check_balance, state=tk.DISABLED)
-# check_balance_button.pack()
-
-#  # Create and place the balance label
-# balance_label = tk.Label(root, text='balance: ')
-# balance_label.pack()
-
-# root.mainloop()
-
-# conn.close()
-
-# import mysql.connector
-
-# connection = mysql.connector.connect(user = "root", database = "elite_bank", password="Nafisa@2008")
-
-# import tkinter as tk
-# import sqlite3
-
-# # Connect to the SQLite database
-# conn = sqlite3.connect('elite_bank.db')
-# cursor = conn.cursor()
-
-# def login():
-#     # Get the entered username and password
-#     entered_username = username_entry.get()
-#     entered_password = password_entry.get()
-
-#     # Check if the user exists in the database
-#     query = f"SELECT * FROM users WHERE username = '{entered_username}' AND password = '{entered_password}'"
-#     cursor.execute(query)
-#     user = cursor.fetchone()
-
-#     if user:
-#         # Save the user ID and balance for future reference
-#         global user_id, balance
-#         user_id = user[0]
-#         balance = user[3]
-
-#         # Update the balance label
-#         balance_label.config(text=f"Balance: ${balance}")
-
-#         # Enable the transaction buttons
-#         transfer_button.config(state=tk.NORMAL)
-#         check_balance_button.config(state=tk.NORMAL)
-#     else:
-#         # Show an error message
-#         error_label.config(text="Invalid username or password")
-
-# def transfer():
-#     # Get the entered recipient username and transfer amount
-#     recipient_username = recipient_entry.get()
-#     transfer_amount = int(amount_entry.get())
-
-#     # Check if the recipient exists in the database
-#     query = f"SELECT * FROM users WHERE username = '{recipient_username}'"
-#     cursor.execute(query)
-#     recipient = cursor.fetchone()
-
-#     if recipient:
-#         recipient_id = recipient[0]
-#         recipient_balance = recipient[3]
-
-#         if balance >= transfer_amount:
-#             # Update the sender's balance
-#             balance -= transfer_amount
-#             query = f"UPDATE users SET balance = {balance} WHERE id = {user_id}"
-#             cursor.execute(query)
-
-#             # Update the recipient's balance
-#             recipient_balance += transfer_amount
-#             query = f"UPDATE users SET balance = {recipient_balance} WHERE id = {recipient_id}"
-#             cursor.execute(query)
-
-#             # Commit the changes to the database
-#             conn.commit()
-
-#             # Update the balance label
-#             balance_label.config(text=f"Balance: ${balance}")
-#         else:
-#             error_label.config(text="Insufficient funds")
-#     else:
-#         error_label.config(text="Recipient not found")
-
-# def check_balance():
-#     balance_label.config(text=f"Balance: ${balance}")
-
-# # Create the main root
-# root = tk.Tk()
-# root.title("WELCOME TO THE BANK")
-
-# # Create and place the login form widgets
-# username_label = tk.Label(root, text="Username:")
-# username_label.pack()
-# username_entry = tk.Entry(root)
-# username_entry.pack()
-
-# password_label = tk.Label(root, text="Password:")
-# password_label.pack()
-# password_entry = tk.Entry(root, show="*")
-# password_entry.pack()
-
-# login_button = tk.Button(root, text="Login", command=login)
-# login_button.pack()
-
-# # Create and place the transaction form widgets
-# recipient_label = tk.Label(root, text="Recipient:")
-# recipient_label.pack()
-# recipient_entry = tk.Entry(root)
-# recipient_entry.pack()
-
-# amount_label = tk.Label(root, text="Amount:")
-# amount_label.pack()
-# amount_entry = tk.Entry(root)
-# amount_entry.pack()
-
-# transfer_button = tk.Button(root, text="Transfer", command=transfer, state=tk.DISABLED)
-# transfer_button.pack()
-
-# check_balance_button = tk.Button(root, text="Check Balance", command=check_balance, state=tk.DISABLED)
-# check_balance_button.pack()
-
-# # Create and place the balance label
-# balance_label = tk.Label(root, text='balance: ')
-# balance_label.pack()
-
-# root.mainloop()
-
-# conn.close()
